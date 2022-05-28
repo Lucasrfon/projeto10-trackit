5cdebfdba576370392fa98c749205f740c3a329e
Lucas Fonseca <lucasrfon@gmail.com>
2022-05-28 02:30:52 -0300

    Tela login implementada

--- src/Routes/Login.js
+++ src/Routes/Login.js
@@ -1,19 +1,43 @@
+import axios from 'axios';
 import { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 import logo from '../images/logo.svg';
 
 export default function Login() {
-    const [login, setLogin] = useState({email: "", password: ""})
+    const [login, setLogin] = useState({email: "", password: ""});
+    const [able, setAble] = useState(true);
+    const navigate = useNavigate();
+
+    function sentRequest(event) {
+        event.preventDefault();
+        setAble(!able);
+        const promise = axios.post('https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit/auth/login', {email: login.email, password: login.password});
+        promise.catch(badRegister);
+        promise.then(navigate('/hoje'));
+    }
+
+    function badRegister() {
+        alert('Falha no Login! Tente novamente!');
+        navigate('/');
+    }
+
     return (
         <Container>
             <img src={logo} alt='Logo' />
-            <form>
-                <input type="email" placeholder="email" required value={login.email} onChange={e => setLogin({...login, email: e.target.value})} />
-                <input type="password" placeholder="senha" required value={login.password} onChange={e => setLogin({...login, password: e.target.value})} />
-                {console.log(login)}
-                <button>Entrar</button>
-            </form>
+            {able ?
+                <form onSubmit={sentRequest}>
+                    <input type="email" placeholder="email" required value={login.email} onChange={e => setLogin({...login, email: e.target.value})} />
+                    <input type="password" placeholder="senha" required value={login.password} onChange={e => setLogin({...login, password: e.target.value})} />
+                    <button typeof='submit'>Entrar</button>
+                </form>
+                :
+                <form>
+                    <input type="email" placeholder="email" required value={login.email} onChange={e => setLogin({...login, email: e.target.value})} disabled />
+                    <input type="password" placeholder="senha" required value={login.password} onChange={e => setLogin({...login, password: e.target.value})} disabled />
+                    <button typeof='submit' disabled>Entrar</button>
+                </form>
+            }
             <Link to="/cadastro" style={{ textDecoration: 'none' }}>
                 <span>Não tem uma conta? Cadastre-se!</span>
             </Link>
--- src/Routes/Register.js
+++ src/Routes/Register.js
@@ -6,8 +6,8 @@ import logo from '../images/logo.svg';
 
 export default function Register() {
     const [register, setRegister] = useState({email: "", password: "", name: "", pic: ""});
-    const [able, setAble] = useState(true)
-    const navigate = useNavigate()
+    const [able, setAble] = useState(true);
+    const navigate = useNavigate();
 
     function sentRequest(event) {
         event.preventDefault();
@@ -31,21 +31,21 @@ export default function Register() {
         <Container>
             <img src={logo} alt='Logo' />
             {able ?
-            <form onSubmit={sentRequest}>
-                <input type="email" placeholder="email" required value={register.email} onChange={e => setRegister({...register, email: e.target.value})} />
-                <input type="password" placeholder="senha" required value={register.password} onChange={e => setRegister({...register, password: e.target.value})} />
-                <input type="text" placeholder="nome" required value={register.name} onChange={e => setRegister({...register, name: e.target.value})} />
-                <input type="text" placeholder="foto" required value={register.foto} onChange={e => setRegister({...register, pic: e.target.value})} />
-                <button typeof='submit'>Cadastrar</button>
-            </form>
-            :
-            <form onSubmit={sentRequest}>
-                <input type="email" placeholder="email" required value={register.email} onChange={e => setRegister({...register, email: e.target.value})} disabled />
-                <input type="password" placeholder="senha" required value={register.password} onChange={e => setRegister({...register, password: e.target.value})} disabled />
-                <input type="text" placeholder="nome" required value={register.name} onChange={e => setRegister({...register, name: e.target.value})} disabled />
-                <input type="text" placeholder="foto" required value={register.foto} onChange={e => setRegister({...register, pic: e.target.value})} disabled />
-                <button typeof='submit' disabled>Cadastrar</button>
-            </form>
+                <form onSubmit={sentRequest}>
+                    <input type="email" placeholder="email" required value={register.email} onChange={e => setRegister({...register, email: e.target.value})} />
+                    <input type="password" placeholder="senha" required value={register.password} onChange={e => setRegister({...register, password: e.target.value})} />
+                    <input type="text" placeholder="nome" required value={register.name} onChange={e => setRegister({...register, name: e.target.value})} />
+                    <input type="text" placeholder="foto" required value={register.foto} onChange={e => setRegister({...register, pic: e.target.value})} />
+                    <button typeof='submit'>Cadastrar</button>
+                </form>
+                :
+                <form onSubmit={sentRequest}>
+                    <input type="email" placeholder="email" required value={register.email} onChange={e => setRegister({...register, email: e.target.value})} disabled />
+                    <input type="password" placeholder="senha" required value={register.password} onChange={e => setRegister({...register, password: e.target.value})} disabled />
+                    <input type="text" placeholder="nome" required value={register.name} onChange={e => setRegister({...register, name: e.target.value})} disabled />
+                    <input type="text" placeholder="foto" required value={register.foto} onChange={e => setRegister({...register, pic: e.target.value})} disabled />
+                    <button typeof='submit' disabled>Cadastrar</button>
+                </form>
             }
             <Link to="/" style={{ textDecoration: 'none' }}>
                 <span>Já tem uma conta? Faça login!</span>
