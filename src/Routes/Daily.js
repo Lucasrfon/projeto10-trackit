9966484778a87c3c2012612654e7d7b83a4bb1b3
Lucas Fonseca <lucasrfon@gmail.com>
2022-05-29 22:48:47 -0300

    Habitos faltando deletar e hoje faltando selecionar

--- src/Routes/Daily.js
+++ src/Routes/Daily.js
@@ -1,13 +1,58 @@
 import styled from "styled-components";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import UserContext from "../contexts/UserContext";
+
+import { useContext, useEffect, useState } from "react";
+import ProgressContext from "../contexts/ProgressContext";
+import axios from "axios";
 
 export default function Daily() {
+    const { user } = useContext(UserContext);
+    const config = {headers: {Authorization: `Bearer ${user.token}`}};
+    const { progress, setProgress } = useContext(ProgressContext);
+    const dayjs = require('dayjs');
+    const [habits, setHabits] = useState([]);
+
+    useEffect(() => {
+        const promise = axios.get('https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit/habits/today', config);
+        promise.then(renderHabits)
+    }, []);
+
+    function renderHabits (promise) {
+        setHabits(promise.data)
+    }
+
+    function select (id, done) {
+        console.log(id);
+        console.log(done);
+        if(done === false){
+            const promise = axios.post(`https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit/habits/${id}/check`, config);
+            promise.then(() => console.log('oi'))
+        } 
+    }
+
     return (
         <>
             <Header />
             <Container>
-
+                <h1>{dayjs().format('dddd, DD/MM')}</h1>
+                {progress === 0 ? <Progresso color="#BABABA">Nenhum hábito concluído ainda</Progresso> : <Progresso color="#8FC549">{progress}% dos hábitos concluídos</Progresso>}
+                <Habits>
+                    {console.log(habits)}
+                    {habits.map((a) => 
+                        <Habit key={a.id}>
+                            <div>
+                                <h3>{a.name}</h3>
+                                <p>sequência atual: {a.currentSequence} dias</p>
+                                <p>seu recorde: {a.highestSequence} dias</p>
+                            </div>
+                            <StyleIcon color={a.done ? "#8FC549" : "#E7E7E7"}>
+                                <ion-icon name="checkbox" onClick={() => select(a.id, a.done)}></ion-icon>
+                            </StyleIcon>
+                        </Habit>
+                    )}
+                </Habits>
             </Container>
             <Footer />
         </>
@@ -16,8 +61,48 @@ export default function Daily() {
 
 const Container = styled.div`
     background-color: #F2F2F2;
-    height: 100vh;
+    min-height: 100vh;
     padding: 100px 15px;
     font-size: 18px;
     color: #666666;
+
+h1 {
+    color: #126BA5;
+    font-size: 24px;
+    margin-bottom: 10px;
+}
+`
+
+const Progresso = styled.p`
+    color: ${props => props.color};
+    font-size: 18px;
+`
+
+const Habits = styled.div`
+    margin-top: 30px;
+`
+
+const Habit = styled.div`
+    margin-bottom: 10px;
+    background-color: white;
+    border-radius: 5px;
+    min-height: 95px;
+    padding: 13px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+h3 {
+    margin-bottom: 8px;
+    font-size: 20px;
+}
+
+p {
+    font-size: 12px;
+}
+`
+
+const StyleIcon = styled.span`
+    font-size: 68px;
+    color: ${props => props.color}
 `
